0f22a642dc92c052627d22d804de4985cd091173
Gideonnz <gideons15@yahoo.com>
2026-08-26 04:56:57 +0700

    Update saldo.py

--- Finance/modules/saldo.py
+++ Finance/modules/saldo.py
@@ -1,7 +1,7 @@
 from telethon import events
 from Finance import client
 from Finance.status import user
-from Finance.database.saldo import atur_saldo_awal
+from Finance.database.saldo_db import atur_saldo_awal
 
 @client.on(events.NewMessage(pattern="^[/.!]saldo_awal"))
 async def saldo_awal(event):
