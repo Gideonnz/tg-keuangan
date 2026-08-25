42771d6dbff1b2aa1379b8ab2ee396cf945f3086
Gideonnz <gideons15@yahoo.com>
2026-08-25 22:17:07 +0700

    Update excel.py

--- Finance/modules/excel.py
+++ Finance/modules/excel.py
@@ -1,7 +1,24 @@
+import pandas as pd
 from telthon import events
 from Finance import client
 from Finance.status import user
-from Finance.database.excel import buat_excel
+from Finance.database.transaksi import semua_transaksi
+
+def buat_excel():
+
+    data = semua_transaksi()
+
+    df = pd.DataFrame(
+        data,
+        columns=["Tanggal", "Tipe", "Kategori", "Nominal", "Keterangan"
+        ]
+    )
+
+    nama_file = "rekap_keuangan.xlsx"
+
+    df.to_excel(nama_file, index=False)
+
+    return nama_file
 
 @client.on(events.NewMessage(pattern="^[/.!]excel"))
 async def excel(event):
