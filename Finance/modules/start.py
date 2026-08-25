169eb03b70bf50ba0430ae16cf8a6254d0adb974
Gideonnz <gideons15@yahoo.com>
2026-08-25 13:09:03 +0700

    Create start.py

---
+++ Finance/modules/start.py
@@ -0,0 +1,22 @@
+from Finance import client
+from telethon import events, Button
+
+START_TEXT = """
+**Hi [{}](tg://user?id={})!**
+
+Aku adalah Bot Pencatat Keuangan Kamu
+Saya Bisa Mencatat Pengeluaran dan Pemasukan Anda Hanya Dari Ketikan.
+
+Klik Tombol Di Bawah Ini Untuk Menu Bantuan.
+"""
+
+@client.on(events.NewMessage(pattern=r"^[/.!]start"))
+async def start(event):
+
+    if not user(event):
+        return
+
+
+    await event.respond(START_TEXT.format(event.sender.first_name, event.sender_id),
+        buttons=[[Button.inline("Bantuan", data="bantuan")]]
+                       )
