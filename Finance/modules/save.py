7051f4a0e77fb847e3fa91dfcf8b40d3ff468ff4
Gideonnz <gideons15@yahoo.com>
2026-08-25 21:38:29 +0700

    Create save.py

---
+++ Finance/modules/save.py
@@ -0,0 +1,60 @@
+from telethon import events
+from Finance.database.transaksi import tambah_transaksi
+
+
+
+async def simpan(event, tipe):
+
+    try:
+
+        data = event.raw_text.split(
+            " ",
+            3
+        )
+
+
+        nominal = int(data[1])
+
+        kategori = data[2]
+
+        keterangan = data[3]
+
+
+        saldo = tambah_transaksi(
+          tipe,
+          kategori,
+          nominal,
+          keterangan
+        )
+
+
+        await event.reply(
+f"""
+Berhasil disimpan.
+
+Jenis:
+{tipe}
+
+Kategori:
+{kategori}
+
+Nominal:
+Rp {nominal:,}
+
+Sisa Saldo:
+Rp {saldo:,}
+"""
+)
+
+
+    except:
+
+        await event.reply(
+"""
+Format salah.
+
+Contoh:
+
+/masuk 100000 Gaji Bonus
+"""
+        )
