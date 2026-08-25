960de84c8588e10f11aeb1276cf1d7f76334278d
Gideonnz <gideons15@yahoo.com>
2026-08-25 21:58:53 +0700

    Update __init__.py

--- Finance/modules/database/__init__.py
+++ Finance/modules/database/__init__.py
@@ -4,3 +4,32 @@ DB_NAME = "keuangan.db"
 
 def koneksi():
   return sqlite3.connect(DB_NAME)
+
+def buat_database():
+
+    conn = koneksi()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS transaksi (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        tanggal TEXT,
+        tipe TEXT,
+        kategori TEXT,
+        nominal INTEGER,
+        keterangan TEXT,
+        saldo_setelah INTEGER
+    )
+    """)
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pengaturan (
+        id INTEGER PRIMARY KEY,
+        saldo_awal INTEGER
+    )
+    """)
+
+
+    conn.commit()
+    conn.close()
