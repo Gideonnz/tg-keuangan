88f946cbb2a77d646ea8c476db100624ede25f2e
Gideonnz <gideons15@yahoo.com>
2026-08-25 21:52:23 +0700

    Create reset_db.py

---
+++ Finance/modules/reset_db.py
@@ -0,0 +1,29 @@
+from telethon import events, Button
+from Finance import client
+from Finance.status import user
+from Finance.database.reset_db import reset_database
+
+@client.on(events.NewMessage(pattern="^[/.!]reset_db"))
+async def reset_db(event):
+
+    if not user(event):
+        return
+
+    
+    await event.reply("Apakah Anda yakin ingin mereset database?\n\nSetelah dihapus database tidak dapat dipulihkan lagi.",
+         buttons=[[Button.inline("Reset Database", data="reset"), Button.inline("Batal", data="batal")]]
+         )
+
+
+@client.on(events.callbackquery.CallbackQuery(data="reset"))
+async def breset(event):
+    reset_database()
+
+    await event.edit("""Database berhasil direset
+
+                     Semua transaksi dan saldo telah di hapus.""")
+
+@client.on(events.callbackquery.CallbackQuery(data="batal"))
+async def bbatal(event):
+
+    await event.edit("Aksi telah dibatalkan.")
