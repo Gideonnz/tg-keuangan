d45adedcb41872924e260664a4ae77393b811924
Gideonnz <gideons15@yahoo.com>
2026-08-25 11:46:37 +0700

    Update database.py

--- database.py
+++ database.py
@@ -18,37 +18,144 @@ def buat_database():
         tipe TEXT,
         kategori TEXT,
         nominal INTEGER,
-        keterangan TEXT
+        keterangan TEXT,
+        saldo_setelah INTEGER
     )
     """)
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pengaturan (
+        id INTEGER PRIMARY KEY,
+        saldo_awal INTEGER
+    )
+    """)
+
 
     conn.commit()
     conn.close()
 
-def tambah_transaksi(tipe, kategori, nominal, keterangan):
+
+def atur_saldo_awal(nominal):
 
     conn = koneksi()
     cursor = conn.cursor()
+
+
+    cursor.execute(
+        "DELETE FROM pengaturan"
+    )
+
+
+    cursor.execute("""
+    INSERT INTO pengaturan
+    (id, saldo_awal)
+    VALUES (1, ?)
+    """,
+    (nominal,)
+    )
+
+
+    conn.commit()
+    conn.close()
+
+
+def saldo_sekarang():
+
+    conn = koneksi()
+    cursor = conn.cursor()
+
+
+    cursor.execute("""
+    SELECT saldo_awal
+    FROM pengaturan
+    WHERE id=1
+    """)
+
+    data = cursor.fetchone()
+
+
+    saldo_awal = 0
+
+    if data:
+        saldo_awal = data[0]
+
+
+    cursor.execute("""
+    SELECT saldo_setelah
+    FROM transaksi
+    ORDER BY id DESC
+    LIMIT 1
+    """)
+
+
+    terakhir = cursor.fetchone()
+
+
+    conn.close()
+
+
+    if terakhir:
+        return terakhir[0]
+
+    return saldo_awal
+
+
+
+def tambah_transaksi(
+    tipe,
+    kategori,
+    nominal,
+    keterangan
+):
+
+    conn = koneksi()
+    cursor = conn.cursor()
+
+
+    saldo = saldo_sekarang()
+
+
+    if tipe == "MASUK":
+        saldo += nominal
+
+    else:
+        saldo -= nominal
+
+
 
     tanggal = datetime.now().strftime(
         "%Y-%m-%d %H:%M:%S"
     )
 
+
     cursor.execute("""
     INSERT INTO transaksi
-    (tanggal, tipe, kategori, nominal, keterangan)
-    VALUES (?, ?, ?, ?, ?)
+    (
+    tanggal,
+    tipe,
+    kategori,
+    nominal,
+    keterangan,
+    saldo_setelah
+    )
+    VALUES (?,?,?,?,?,?)
     """,
     (
         tanggal,
         tipe,
         kategori,
         nominal,
-        keterangan
+        keterangan,
+        saldo
     ))
+
 
     conn.commit()
     conn.close()
+
+
+    return saldo
 
 def ambil_laporan_bulan():
 
