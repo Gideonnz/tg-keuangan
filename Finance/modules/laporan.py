d99ba3a8e54a4bcbff5c261f3b40a43a4dd7a5a7
Gideonnz <gideons15@yahoo.com>
2026-08-25 21:44:20 +0700

    Create laporan.py

---
+++ Finance/modules/laporan.py
@@ -0,0 +1,31 @@
+from telethon import events
+from Finance import client
+from Finance.status import user
+from Finance.database.laporan import ambil_laporan_bulan
+
+@client.on(events.NewMessage(pattern="^[/.!]laporan"))
+async def laporan(event):
+
+    if not user(event):
+        return
+
+
+    masuk, keluar = ambil_laporan_bulan()
+
+    saldo = masuk - keluar
+
+
+    await event.reply(
+f"""
+Laporan Bulan Ini
+
+Pemasukan:
+Rp {masuk:,}
+
+Pengeluaran:
+Rp {keluar:,}
+
+Saldo:
+Rp {saldo:,}
+"""
+    )
