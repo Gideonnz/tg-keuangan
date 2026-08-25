058c88b00983f0520b61043bed9cbe97d468fd8f
Gideonnz <gideons15@yahoo.com>
2026-08-25 22:06:26 +0700

    Create transaksi.py

---
+++ Finance/modules/database/transaksi.py
@@ -0,0 +1,148 @@
+from datetime import datetime
+from . import koneksi
+from .saldo import saldo_sekarang
+
+def tambah_transaksi(
+    tipe,
+    kategori,
+    nominal,
+    keterangan
+):
+
+    conn = koneksi()
+    cursor = conn.cursor()
+
+
+    saldo = saldo_sekarang()
+
+
+    if tipe == "MASUK":
+        saldo += nominal
+
+    else:
+        saldo -= nominal
+
+
+
+    tanggal = datetime.now().strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
+
+
+    cursor.execute("""
+    INSERT INTO transaksi
+    (
+    tanggal,
+    tipe,
+    kategori,
+    nominal,
+    keterangan,
+    saldo_setelah
+    )
+    VALUES (?,?,?,?,?,?)
+    """,
+    (
+        tanggal,
+        tipe,
+        kategori,
+        nominal,
+        keterangan,
+        saldo
+    ))
+
+
+    conn.commit()
+    conn.close()
+
+
+    return saldo
+
+
+def semua_transaksi():
+
+    conn = koneksi()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT tanggal, tipe, kategori, nominal, keterangan
+    FROM transaksi
+    ORDER BY id DESC
+    """)
+
+    data = cursor.fetchall()
+
+    conn.close()
+
+    return data
+
+def hapus_transaksi(id_transaksi):
+
+    conn = koneksi()
+    cursor = conn.cursor()
+
+
+    cursor.execute(
+        """
+        DELETE FROM transaksi
+        WHERE id=?
+        """,
+        (id_transaksi,)
+    )
+
+
+    berhasil = cursor.rowcount > 0
+
+
+    conn.commit()
+    conn.close()
+
+
+    return berhasil
+
+
+
+def cek_transaksi(id_transaksi):
+
+    conn = koneksi()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT
+        id,
+        tanggal,
+        tipe,
+        kategori,
+        nominal,
+        keterangan,
+        saldo_setelah
+    FROM transaksi
+    WHERE id=?
+    """,
+    (id_transaksi,))
+
+
+    data = cursor.fetchone()
+
+    conn.close()
+
+    return data
+
+
+def total_transaksi():
+
+    conn = koneksi()
+    cursor = conn.cursor()
+
+
+    cursor.execute("""
+    SELECT COUNT(*)
+    FROM transaksi
+    """)
+
+
+    total = cursor.fetchone()[0]
+
+
+    conn.close()
+
+    return total
