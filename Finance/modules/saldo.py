9764a83c494f6d1535d760f2d7d1c8d7ec348c29
Gideonnz <gideons15@yahoo.com>
2026-08-25 21:42:27 +0700

    Create saldo.py

---
+++ Finance/modules/saldo.py
@@ -0,0 +1,47 @@
+from telethon import events
+from Finance import client
+from Finance.status import user
+from Finance.database.saldo import atur_saldo_awal
+
+@client.on(events.NewMessage(pattern="^[/.!]saldo_awal"))
+async def saldo_awal(event):
+
+    if not user(event):
+        return
+
+
+    try:
+
+        data = event.raw_text.split()
+
+
+        nominal = int(data[1])
+
+
+        atur_saldo_awal(
+            nominal
+        )
+
+
+        await event.reply(
+f"""
+Saldo awal berhasil dibuat.
+
+Saldo awal:
+Rp {nominal:,}
+"""
+        )
+
+
+    except:
+
+        await event.reply(
+"""
+Format:
+
+/saldo 1000000
+
+Contoh:
+/saldo 5000000
+"""
+        )
