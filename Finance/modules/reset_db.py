e585d08761bc116d57a3c2e555251b480e92f38d
Gideonnz <gideons15@yahoo.com>
2026-08-26 03:45:33 +0700

    Update reset_db.py

--- Finance/modules/reset_db.py
+++ Finance/modules/reset_db.py
@@ -1,7 +1,7 @@
 from telethon import events, Button
 from Finance import client
 from Finance.status import user
-from Finance.database.reset_db import reset_database
+from Finance.database.reset_db import reset_database, reset_data
 
 @client.on(events.NewMessage(pattern="^[/.!]reset_db"))
 async def reset_db(event):
@@ -11,19 +11,44 @@ async def reset_db(event):
 
     
     await event.reply("Apakah Anda yakin ingin mereset database?\n\nSetelah dihapus database tidak dapat dipulihkan lagi.",
-         buttons=[[Button.inline("Reset Database", data="reset"), Button.inline("Batal", data="batall")]]
+         buttons=[[Button.inline("Reset Database", data="reset"), Button.inline("Batal", data="bbatal")]]
          )
 
 
-@client.on(events.callbackquery.CallbackQuery(data="reset"))
+@client.on(events.CallbackQuery(data="reset"))
 async def breset(event):
     reset_database()
 
-    await event.edit("""Database berhasil direset
+    await event.edit("""
+       Database berhasil direset.
 
-                     Semua transaksi dan saldo telah di hapus.""")
+       Semua data dalam database telah di hapus.""")
 
-@client.on(events.callbackquery.CallbackQuery(data="batall"))
+@client.on(events.CallbackQuery(data="bbatal"))
 async def bbatal(event):
 
     await event.edit("Aksi telah dibatalkan.")
+
+
+@client.on(events.NewMessage(pattern="^[/.!]reset"))
+async def reset(event):
+
+    if not user(event):
+        return
+
+    
+    await event.reply("Apakah Anda yakin ingin menghapus riwayat transaksi?",
+         buttons=[[Button.inline("Reset Data", data="reset2"), Button.inline("Batal", data="bbatal2")]]
+                     )
+
+
+@client.on(events.CallbackQuery(data="reset2"))
+async def breset2(event):
+    reset_data()
+
+    await event.reply("✅ Semua riwayat transaksi berhasil dihapus.")
+
+@client.on(events.CallbackQuery(data="bbatal2"))
+async def bbatal2(event):
+
+    await event.reply("Aksi telah dibatalkan.")
