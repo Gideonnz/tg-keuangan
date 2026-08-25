167c915180bc9e8982c4fc396a93522d5ebcac17
Gideonnz <gideons15@yahoo.com>
2026-08-25 22:07:58 +0700

    Create laporan.py

---
+++ Finance/modules/database/laporan.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from . import koneksi
+
+def ambil_laporan_bulan():
+
+    conn = koneksi()
+    cursor = conn.cursor()
+
+    bulan = datetime.now().strftime("%Y-%m")
+
+    cursor.execute("""
+    SELECT tipe, SUM(nominal)
+    FROM transaksi
+    WHERE tanggal LIKE ?
+    GROUP BY tipe
+    """,
+    (
+        bulan+"%",
+    ))
+
+    data = cursor.fetchall()
+
+    conn.close()
+
+    pemasukan = 0
+    pengeluaran = 0
+
+    for tipe,total in data:
+
+        if tipe=="MASUK":
+            pemasukan = total
+
+        else:
+            pengeluaran = total
+
+    return pemasukan, pengeluaran
