620662757209bbf19c12698fc1a5ec008fcff331
Gideonnz <gideons15@yahoo.com>
2026-08-25 23:07:06 +0700

    Create __main__.py

---
+++ Finance/__main__.py
@@ -0,0 +1,16 @@
+import logging
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+
+def main():
+    buat_database()
+    print("Bot Bekerja Dengan Baik.")
+
+    client.start()
+    client.run_until_disconnected()
+
+
+
+if __name__=="__main__":
+    main()
